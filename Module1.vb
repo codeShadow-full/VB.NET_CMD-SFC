1ca71e155842e3a98f3ab5b8bad9dbfd68bdc69e
code_shadow <code.shadow9999@gmail.com>
2025-02-25 11:05:24 -0500

    Fix:Disable auto redirect to 64bit

--- Module1.vb
+++ Module1.vb
@@ -1,37 +1,43 @@
 ﻿Imports System.Diagnostics
+Imports System.IO
+Imports System.Runtime.InteropServices
 
 Module Module1
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Function Wow64DisableWow64FsRedirection(ByRef oldValue As IntPtr) As Boolean
+    End Function
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Function Wow64RevertWow64FsRedirection(ByVal oldValue As IntPtr) As Boolean
+    End Function
     Sub Main()
-        Try
-            Dim proc As New Process()
-            proc.StartInfo.FileName = "cmd.exe"
-            proc.StartInfo.Verb = "runas"
-            proc.StartInfo.Arguments = "/c sfc /scannow"
-            proc.StartInfo.RedirectStandardOutput = True
-            proc.StartInfo.RedirectStandardError = True
-            proc.StartInfo.UseShellExecute = False
-            proc.StartInfo.CreateNoWindow = True
+        Dim oldValue As IntPtr
 
-            proc.Start()
-            Console.WriteLine("Start scanning")
+        If Wow64DisableWow64FsRedirection(oldValue) Then
+            Console.WriteLine("Start scanning...")
+            Dim psi As New ProcessStartInfo()
+            psi.FileName = "cmd.exe"
+            psi.Arguments = "/c sfc /scannow"
+            psi.UseShellExecute = False
+            psi.RedirectStandardOutput = True
+            psi.CreateNoWindow = True
 
-            Dim output As String = proc.StandardOutput.ReadToEnd()
-            Dim errorOutput As String = proc.StandardError.ReadToEnd()
-
-            proc.WaitForExit()
-
-            Console.WriteLine("SFC Output:")
-            Console.WriteLine(output)
-
-            Console.WriteLine("End scanning")
-
-            If Not String.IsNullOrEmpty(errorOutput) Then
-                Console.WriteLine("Errors:")
-                Console.WriteLine(errorOutput)
-            End If
-
-        Catch ex As Exception
-            Console.WriteLine("Error: " & ex.Message)
-        End Try
+            Try
+                Dim process As Process = Process.Start(psi)
+                Dim reader As StreamReader = process.StandardOutput
+                While Not process.HasExited
+                    If Not reader.EndOfStream Then
+                        Dim output As String = reader.ReadLine()
+                        If output IsNot Nothing Then
+                            Console.WriteLine(output)
+                        End If
+                    End If
+                End While
+                Console.WriteLine("End scanning.")
+            Catch ex As Exception
+                Console.WriteLine("Error: " & ex.Message)
+            End Try
+        End If
     End Sub
 End Module
